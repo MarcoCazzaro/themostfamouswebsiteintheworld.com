d516fb11251cf83e683070640f48bff502df24e2
Marco Cazzaro <marco.cazzaro.0@gmail.com>
2022-09-06 18:11:30 +0200

    .

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,9 +7,13 @@
     <div class="flex justify-center content-center min-w-screen">
         <div class="flex items-center justify-center p-8 flex-wrap">
             <p>Join us, if you want to be in The Most Famous Website In The World.</p>
-            <div class="p-8">
-                <x-jet-button type="submit" class="w-24 m-4">Join</x-jet-button>
-                <x-jet-button type="submit" class="w-24 m-4">Login</x-jet-button>
+            <div class="flex p-8">
+                <form method="GET" action="{{ route('register') }}">
+                    <x-jet-button type="submit" class="w-24 m-4">Join</x-jet-button>
+                </form>
+                <form method="GET" action="{{ route('login') }}">
+                    <x-jet-button type="submit" class="w-24 m-4">Login</x-jet-button>
+                </form>
             </div>
         </div>
     </div>
