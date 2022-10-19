64dbfe4fb8aa9ff29a922a227ca1a96bd94aecaf
Marco Cazzaro <marco.cazzaro.0@gmail.com>
2022-10-19 12:46:07 +0200

    .

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -3,7 +3,6 @@
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ $user->name }}
         </h2>
-        <div class="ssnail-points">{{ $user->total_famous_points }}</div>
     </x-slot>
 
     <div class="bg-white shadow fixed bottom-0 w-full transition-height duration-500 ease-in-out h-20 hover:h-96">
