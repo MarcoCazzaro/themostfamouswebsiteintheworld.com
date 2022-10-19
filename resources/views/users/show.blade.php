9a8764c9223426ba7dd3e88fde8d42908d7e0fd5
Marco Cazzaro <marco.cazzaro.0@gmail.com>
2022-10-19 13:04:21 +0200

    .

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -5,7 +5,7 @@
         </h2>
     </x-slot>
 
-    <div class="bg-white shadow fixed bottom-0 w-full transition-height duration-500 ease-in-out h-20 hover:h-96">
+    <div class="bg-white shadow fixed bottom-0 w-full transition-height duration-500 ease-in-out h-20 hover:h-64 sm:hover:h-48">
         <div class="max-w-7xl mx-auto py-3 px-6 lg:px-8">
             @livewire('increment-famousness', compact('user'))
         </div>
