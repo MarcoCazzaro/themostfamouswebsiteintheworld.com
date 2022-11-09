b4619d031e1ebbcda8dca8981d05b1a02f59cc38
Marco Cazzaro <marco.cazzaro.0@gmail.com>
2022-11-09 16:49:32 +0100

    Profile photo letters

--- app/Models/User.php
+++ app/Models/User.php
@@ -159,4 +159,13 @@ class User extends Authenticatable implements MustVerifyEmail
             ->having('sender_id', $this->id);
         return $users->count();
     }
+
+    protected function defaultProfilePhotoUrl()
+    {
+        $name = trim(collect(explode(' ', $this->name))->map(function ($segment) {
+            return mb_substr($segment, 0, 1);
+        })->join(' '));
+
+        return 'https://ui-avatars.com/api/?name='.urlencode($name).'&color=f59e0b&background=fef3c7';
+    }
 }
