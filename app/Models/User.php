fc47fa54695979d0b170b536b07ec5af7e6838a5
Marco Cazzaro <marco.cazzaro.0@gmail.com>
2022-11-09 17:31:30 +0100

    Unknown user icon

--- app/Models/User.php
+++ app/Models/User.php
@@ -162,10 +162,7 @@ class User extends Authenticatable implements MustVerifyEmail
 
     protected function defaultProfilePhotoUrl()
     {
-        $name = trim(collect(explode(' ', $this->name))->map(function ($segment) {
-            return mb_substr($segment, 0, 1);
-        })->join(' '));
-
-        return 'https://ui-avatars.com/api/?name='.urlencode($name).'&color=f59e0b&background=fef3c7';
+        /* overriding Laravel default function */
+        return asset('img/user.png');
     }
 }
--- resources/views/partials/user-card.blade.php
+++ resources/views/partials/user-card.blade.php
@@ -1,13 +1,9 @@
 <?php
 if($user ?? false) {
-    $profile_photo_path = asset('img/logo-squared.png');
-    if ($user->profile_photo_path ?? false) {
-        $profile_photo_path = Storage::url($user->profile_photo_path);
-    }
     ?>
     <div class="w-full max-w-sm bg-white rounded-lg border border-gray-200 shadow-md">
         <div class="flex flex-col items-center p-8">
-            <img class="mb-3 w-24 h-24 rounded-full border border-amber-300" src="{{  $profile_photo_path }}" alt="{{ $user->name }}">
+            <img class="mb-3 w-24 h-24 rounded-full border border-amber-300" src="{{  $user->profile_photo_url }}" alt="{{ $user->name }}">
             <h5 class="mb-1 text-xl font-medium text-gray-900">{{ $user->name }}</h5>
             <div class="ssnail-points text-xs flex items-center">
                 <span>{{ $user->total_famous_points }}</span>
