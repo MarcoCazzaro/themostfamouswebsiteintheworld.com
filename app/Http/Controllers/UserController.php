4fcc3a15648b722216d3caff1063a38f9d705c15
Marco Cazzaro <marco.cazzaro.0@gmail.com>
2022-12-01 16:48:35 +0100

    Cache

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -86,52 +86,72 @@ class UserController extends Controller
 
     public function most_famous_people()
     {
-        $most_famous_tags = Tag::orderByFamousPointsReceived()->take('67')->get();
-        $most_famous_people_by_popular_tag = [];
-        $points_by_tag = FamousPoint::selectRaw('taggables.taggable_id as t_user_id, taggables.tag_id as t_tag_id, sum(ajeje) as worship_amount')
-                ->join('users', 'famous_points.user_id', '=', 'users.id')
-                ->join('taggables', 'users.id', '=', 'taggables.taggable_id')
-                ->where('taggables.taggable_type', User::class)
-                ->groupBy('t_user_id', 't_tag_id');
-        foreach ($most_famous_tags as $tag) {
-            $most_famous_people_by_popular_tag[$tag->id] = User::select('users.*', 'points_by_tag.worship_amount')
-                ->whereHas('tags', function (Builder $query) use ($tag) {
-                    $query->where('taggables.tag_id', $tag->id);
-                })
-                ->joinSub($points_by_tag, 'points_by_tag', function ($join) {
-                    $join->on('users.id', '=', 'points_by_tag.t_user_id');
-                })
-                ->where('points_by_tag.t_tag_id', $tag->id)
-                ->orderBy("points_by_tag.worship_amount", "desc")
-                ->limit(5)->get();
-        }
-        $most_famous_people = User::orderByFamousPointsReceived()->take(13)->get();
+        $cache_seconds_ttl = 300;
+        $most_famous_tags_and_people = cache()->remember('most_famous_tags_and_people', $cache_seconds_ttl, function () {
+            $most_famous_tags_and_people = [];
+            $most_famous_tags = Tag::orderByFamousPointsReceived()->take('67')->get();
+            $most_famous_people_by_popular_tag = [];
+            $points_by_tag = FamousPoint::selectRaw('taggables.taggable_id as t_user_id, taggables.tag_id as t_tag_id, sum(ajeje) as worship_amount')
+                    ->join('users', 'famous_points.user_id', '=', 'users.id')
+                    ->join('taggables', 'users.id', '=', 'taggables.taggable_id')
+                    ->where('taggables.taggable_type', User::class)
+                    ->groupBy('t_user_id', 't_tag_id');
+            foreach ($most_famous_tags as $tag) {
+                $most_famous_people_by_popular_tag[$tag->id] = User::select('users.*', 'points_by_tag.worship_amount')
+                    ->whereHas('tags', function (Builder $query) use ($tag) {
+                        $query->where('taggables.tag_id', $tag->id);
+                    })
+                    ->joinSub($points_by_tag, 'points_by_tag', function ($join) {
+                        $join->on('users.id', '=', 'points_by_tag.t_user_id');
+                    })
+                    ->where('points_by_tag.t_tag_id', $tag->id)
+                    ->orderBy("points_by_tag.worship_amount", "desc")
+                    ->limit(5)->get();
+            }
+            $most_famous_tags_and_people['tags'] = $most_famous_tags;
+            $most_famous_tags_and_people['people_by_tag'] = $most_famous_people_by_popular_tag;
+            $most_famous_tags_and_people['people'] = User::orderByFamousPointsReceived()->take(13)->get();
+            return $most_famous_tags_and_people;
+        });
+        $most_famous_tags = $most_famous_tags_and_people['tags'];
+        $most_famous_people_by_popular_tag = $most_famous_tags_and_people['people_by_tag'];
+        $most_famous_people = $most_famous_tags_and_people['people'];
         $title = __('The Most Famous People');
         return view('users.the-most-famous-people', compact('most_famous_tags', 'most_famous_people_by_popular_tag', 'most_famous_people', 'title'));
     }
 
     public function most_famous_fans()
     {
-        $most_famous_tags = Tag::orderByFamousPointsGiven()->take('67')->get();
-        $most_famous_fans_by_popular_tag = [];
-        $points_by_tag = FamousPoint::selectRaw('taggables.taggable_id as t_user_id, taggables.tag_id as t_tag_id, sum(ajeje) as worship_amount')
-                ->join('users', 'famous_points.sender_id', '=', 'users.id')
-                ->join('taggables', 'users.id', '=', 'taggables.taggable_id')
-                ->where('taggables.taggable_type', User::class)
-                ->groupBy('t_user_id', 't_tag_id');
-        foreach ($most_famous_tags as $tag) {
-            $most_famous_fans_by_popular_tag[$tag->id] = User::select('users.*', 'points_by_tag.worship_amount')
-                ->whereHas('tags', function (Builder $query) use ($tag) {
-                    $query->where('taggables.tag_id', $tag->id);
-                })
-                ->joinSub($points_by_tag, 'points_by_tag', function ($join) {
-                    $join->on('users.id', '=', 'points_by_tag.t_user_id');
-                })
-                ->where('points_by_tag.t_tag_id', $tag->id)
-                ->orderBy("points_by_tag.worship_amount", "desc")
-                ->limit(5)->get();
-        }
-        $most_famous_fans = User::orderByFamousPointsGiven()->take(13)->get();
+        $cache_seconds_ttl = 300;
+        $most_famous_tags_and_fans = cache()->remember('most_famous_tags_and_fans', $cache_seconds_ttl, function () {
+            $most_famous_tags_and_fans = [];
+            $most_famous_tags = Tag::orderByFamousPointsGiven()->take('67')->get();
+            $most_famous_fans_by_popular_tag = [];
+            $points_by_tag = FamousPoint::selectRaw('taggables.taggable_id as t_user_id, taggables.tag_id as t_tag_id, sum(ajeje) as worship_amount')
+                    ->join('users', 'famous_points.sender_id', '=', 'users.id')
+                    ->join('taggables', 'users.id', '=', 'taggables.taggable_id')
+                    ->where('taggables.taggable_type', User::class)
+                    ->groupBy('t_user_id', 't_tag_id');
+            foreach ($most_famous_tags as $tag) {
+                $most_famous_fans_by_popular_tag[$tag->id] = User::select('users.*', 'points_by_tag.worship_amount')
+                    ->whereHas('tags', function (Builder $query) use ($tag) {
+                        $query->where('taggables.tag_id', $tag->id);
+                    })
+                    ->joinSub($points_by_tag, 'points_by_tag', function ($join) {
+                        $join->on('users.id', '=', 'points_by_tag.t_user_id');
+                    })
+                    ->where('points_by_tag.t_tag_id', $tag->id)
+                    ->orderBy("points_by_tag.worship_amount", "desc")
+                    ->limit(5)->get();
+            }
+            $most_famous_tags_and_fans['tags'] = $most_famous_tags;
+            $most_famous_tags_and_fans['fans_by_tag'] = $most_famous_fans_by_popular_tag;
+            $most_famous_tags_and_fans['fans'] = User::orderByFamousPointsGiven()->take(13)->get();
+            return $most_famous_tags_and_fans;
+        });
+        $most_famous_tags = $most_famous_tags_and_fans['tags'];
+        $most_famous_fans_by_popular_tag = $most_famous_tags_and_fans['fans_by_tag'];
+        $most_famous_fans = $most_famous_tags_and_fans['fans'];
         $title = __('The Most Famous Fans');
         return view('users.the-most-famous-people', compact('most_famous_tags', 'most_famous_fans_by_popular_tag', 'most_famous_fans', 'title'));
     }
