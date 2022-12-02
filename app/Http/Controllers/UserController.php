3f7acc4255c8dd67d6f96c30a194ef9ed50fd336
Marco Cazzaro <marco.cazzaro.0@gmail.com>
2022-12-02 18:32:03 +0100

    .

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -88,6 +88,7 @@ class UserController extends Controller
     {
         $cache_ttl_seconds = 300;
         $most_famous_tags = cache()->remember('most_famous_tags_received', $cache_ttl_seconds, function () {
+            \Log::info("Refreshing cache yeah yeah");
             return Tag::orderByFamousPointsReceived()->take('67')->get();
         });
         $most_famous_people_by_popular_tag = [];
