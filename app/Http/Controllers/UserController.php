7aa2d913fb8d0236f560700f74f517c6ae0f6e30
Marco Cazzaro <marco.cazzaro.0@gmail.com>
2022-12-05 10:25:07 +0100

    .

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -87,22 +87,16 @@ class UserController extends Controller
     public function most_famous_people()
     {
         $cache_ttl_seconds = 300;
-        $ciao = cache()->remember('caching_test_yeah', $cache_ttl_seconds, function () {
-            return "CIAOOOOO";
-        });
         $most_famous_tags = cache()->remember('most_famous_tags_received', $cache_ttl_seconds, function () {
             return Tag::orderByFamousPointsReceived()->take(67)->get();
         });
         $most_famous_people_by_popular_tag = [];
-        /*
         $points_by_tag = FamousPoint::selectRaw('taggables.taggable_id as t_user_id, taggables.tag_id as t_tag_id, sum(ajeje) as worship_amount')
                 ->join('users', 'famous_points.user_id', '=', 'users.id')
                 ->join('taggables', 'users.id', '=', 'taggables.taggable_id')
                 ->where('taggables.taggable_type', User::class)
-                ->groupBy('t_user_id', 't_tag_id')
-                ->orderBy("worship_amount", "desc")
-                ->limit(5);
-        $most_famous_13_tags = $most_famous_tags->take(1);
+                ->groupBy('t_user_id', 't_tag_id');
+        $most_famous_13_tags = $most_famous_tags->take(13);
         foreach ($most_famous_13_tags as $tag) {
             $most_famous_people_by_popular_tag[$tag->id] = cache()->remember('most_famous_people_by_popular_tag_' . $tag->id, $cache_ttl_seconds, function () use ($points_by_tag, $tag) {
                 return User::select('users.*', 'points_by_tag.worship_amount')
@@ -117,12 +111,10 @@ class UserController extends Controller
                     ->limit(5)->get();
             });
         }
-        */
         $most_famous_people = cache()->remember('most_famous_people', $cache_ttl_seconds, function () {
             return User::orderByFamousPointsReceived()->take(13)->get();
         });
         $title = __('The Most Famous People');
-        dd($most_famous_people);
         return view('users.the-most-famous-people', compact('most_famous_tags', 'most_famous_people_by_popular_tag', 'most_famous_people', 'title'));
     }
 
