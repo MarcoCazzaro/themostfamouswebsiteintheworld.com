bc7fd1ad132d3e85c46cebdb54f69fe8d8831633
Marco Cazzaro <marco.cazzaro.0@gmail.com>
2022-12-05 10:09:03 +0100

    .

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -86,7 +86,10 @@ class UserController extends Controller
 
     public function most_famous_people()
     {
-        dd("CIAO");
+        $ciao = cache()->remember('caching_test_yeah', $cache_ttl_seconds, function () {
+            return "CIAOOOOO";
+        });
+        dd($ciao);
         $cache_ttl_seconds = 300;
         $most_famous_tags = cache()->remember('most_famous_tags_received', $cache_ttl_seconds, function () {
             return Tag::orderByFamousPointsReceived()->take('67')->get();
