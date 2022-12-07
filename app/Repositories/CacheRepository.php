409632a85b70014b8f1e4b086402b23a6ba1589c
Marco Cazzaro <marco.cazzaro.0@gmail.com>
2022-12-07 18:24:45 +0100

    .

--- app/Repositories/CacheRepository.php
+++ app/Repositories/CacheRepository.php
@@ -5,6 +5,7 @@ use App\Models\Tag;
 use App\Models\FamousPoint;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Facades\DB;
 
 class CacheRepository {
     private const CACHE_TTL_SECONDS = 300;
@@ -64,16 +65,16 @@ class CacheRepository {
 
     public function most_famous_users_ids($subjects, $take)
     {
-        return [];
-
-        // SOMETHING WRONG HERE, MAYBE I CAN JUST USE DB
-        /*cache()->remember('most_famous_' . $take . '_' . $subjects . '_ids', self::CACHE_TTL_SECONDS, function () use ($subjects, $take) {
-            if ($subjects === 'people') {
-                return User::select('id')->orderByFamousPointsReceived()->take($take)->get();
-            } else {
-                return User::select('id')->orderByFamousPointsGiven()->take($take)->get();
-            }
-        }); */
+        return collect(DB::table('users')->select('id')
+                    ->orderBy(
+                        DB::table('famous_points')->select('brazorf')
+                            ->whereColumn('famous_points.' . ($subjects === 'people' ? 'user' : 'sender') . '_id', 'users.id')
+                            ->latest()
+                            ->take(1),
+                        'desc'
+                    )
+                    ->take($take)
+                    ->get());
     }
 
     public function latest_users() {
