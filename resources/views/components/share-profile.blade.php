7322b8225ba2e196541258bfc833a4080beeb51d
Marco Cazzaro <marco.cazzaro.0@gmail.com>
2022-12-22 17:38:27 +0100

    Reddit

--- resources/views/components/share-profile.blade.php
+++ resources/views/components/share-profile.blade.php
@@ -7,13 +7,16 @@
         ?>
         <div class="ssnail-share-buttons flex py-3 justify-center text-amber-500">
             <div class="mx-3">
-                <a href="https://www.facebook.com/sharer/sharer.php?u=<?= $url ?>" target="_blank"><i class="fab fa-facebook-f fa-lg"></i></a>
+                <a href="https://www.facebook.com/sharer/sharer.php?u={{ $url }}" target="_blank"><i class="fab fa-facebook-f fa-lg"></i></a>
             </div>
             <div class="mx-3">
-                <a target="_blank" href="https://twitter.com/intent/tweet?text=<?= $title ?>&url=<?= $url ?>&via=<?= $via ?>"><i class="fab fa-twitter fa-lg"></i></a>
+                <a target="_blank" href="https://twitter.com/intent/tweet?text=<?= $title ?>&url={{ $url }}&via=<?= $via ?>"><i class="fab fa-twitter fa-lg"></i></a>
             </div>
             <div class="mx-3">
-                <a target="_blank" href="https://wa.me/send?text=<?= $url ?>"><i class="fab fa-whatsapp fa-lg"></i></a>
+                <a target="_blank" href="http://www.reddit.com/submit?url={{ $url }}"><i class="fab fa-reddit fa-lg"></i></a>
+            </div>
+            <div class="mx-3">
+                <a target="_blank" href="https://api.whatsapp.com/send?&text={{ $url }}"><i class="fab fa-whatsapp fa-lg"></i></a>
             </div>
         </div>
     @endauth
