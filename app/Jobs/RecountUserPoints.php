f274c9b693109e17c16c2a50a216e626825db0d4
Marco Cazzaro <marco.cazzaro.0@gmail.com>
2023-01-04 17:31:29 +0100

    .

--- app/Jobs/RecountUserPoints.php
+++ app/Jobs/RecountUserPoints.php
@@ -9,6 +9,7 @@ use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\DB;
+use Spatie\ResponseCache\Facades\ResponseCache;
 
 class RecountUserPoints implements ShouldQueue
 {
