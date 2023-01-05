03a5aae720621a4d73568894d7e4cf786c2450b7
Marco Cazzaro <marco.cazzaro.0@gmail.com>
2023-01-05 16:14:20 +0100

    .

--- app/Repositories/CacheRepository.php
+++ app/Repositories/CacheRepository.php
@@ -15,9 +15,9 @@ class CacheRepository
     {
         return cache()->remember('most_famous_tags_'.$type, self::CACHE_TTL_SECONDS, function () use ($type) {
             if ($type === 'received') {
-                return Tag::withCount('users')->orderByFamousPointsReceived()->take(67)->get();
+                return Tag::orderByFamousPointsReceived()->take(67)->get();
             } else {
-                return Tag::withCount('users')->orderByFamousPointsGiven()->take(67)->get();
+                return Tag::orderByFamousPointsGiven()->take(67)->get();
             }
         });
     }
