889ceda3e19d25701ac84483cc64f6476728a155
Marco Cazzaro <marco.cazzaro.0@gmail.com>
2023-01-10 17:48:26 +0100

    Social links

--- resources/views/partials/site-footer.blade.php
+++ resources/views/partials/site-footer.blade.php
@@ -7,10 +7,14 @@
                     <p>&copy;{{ date('Y') }} | All rights reserved</p>
                     <p><b class="font-semibold text-amber-500">The Most Famous Website In The World</b> is owned by:</p>
                     <p class="text-sm">Snappysnail di Marco Cazzaro <br> Via IV Novembre 9 22045 Lambrugo (CO) Italy <br> VAT NUMBER: IT03919560130</p>
+                    <div class="ssnail-social-links text-amber-500 mt-4">
+                        <a href="https://twitter.com/tmfwitw" target="_blank" rel="noopener nofollow" class="mr-4"><i class="fab fa-twitter fa-xl"></i></a>
+                        <a href="https://www.facebook.com/profile.php?id=100089300353495" target="_blank" rel="noopener nofollow" class="mr-4"><i class="fab fa-facebook-f fa-xl"></i></a>
+                    </div>
                 </div>
             </div>
         </div>
-        <div class="px-0 md:px-8">
+        <div class="px-0 md:px-8 mt-8 md:mt-0">
             <p>
                 To contact us, please send an email to <img src="{{ asset('img/info-email.png') }}" alt="info at snappysnail dot io" class="invert inline-flex">
             </p>
