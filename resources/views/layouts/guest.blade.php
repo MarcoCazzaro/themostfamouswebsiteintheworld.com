426a6f887f9b28dc5e35c47a939d6fd60ea763ac
Marco Cazzaro <marco.cazzaro.0@gmail.com>
2023-01-14 13:10:58 +0100

    Double font declaration removed

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -6,9 +6,6 @@
         @stack('page-preloads')
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
-
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
         <script src="https://kit.fontawesome.com/fea9be3e02.js" crossorigin="anonymous"></script>
